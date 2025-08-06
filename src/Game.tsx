56663248c9130ea192132cdb03e1a954ff51ce23
최지원 <66864266+chamchi0809@users.noreply.github.com>
2025-08-06 17:59:04 +0900

    Camera Fix

--- src/Game.tsx
+++ src/Game.tsx
@@ -11,32 +11,32 @@ export default function Game() {
 
     return <>
         <ambientLight intensity={1}/>
-        <OrthographicCamera position={[0, 0, -10]}>
+        <OrthographicCamera makeDefault position={[0, 0, 10]} zoom={10}>
             <CameraControls/>
-            <LdtkMap
-                ldtkPath={"/assets/ldtk/test.ldtk"}
-                entityRendererMap={{
-                    "PlayerStart": ({entity, layer, layerPxOffsets, layerPxDimensions}) => {
-                        const worldPx = centerTilePivot(layerPxToWorldPx(entity.px as [number, number], layerPxOffsets, layerPxDimensions), layer.__gridSize);
-                        const worldPos = pxToGridPosition(worldPx, layer.__gridSize);
-
-                        return <RigidBody colliders={"hull"} position={[worldPos[0], worldPos[1], 0]}>
-                            <mesh>
-                                <boxGeometry args={[1, 1, 1]}/>
-                                <meshStandardMaterial color={"#00ff00"}/>
-                            </mesh>
-                        </RigidBody>
-                    }
-                }}
-                tileRendererMap={{
-                    "RECT": (props) => {
-                        return <>
-                            <TileSprite {...props}/>
-                            <TileRectCollider {...props}/>
-                        </>
-                    }
-                }}
-            />
         </OrthographicCamera>
+        <LdtkMap
+            ldtkPath={"/assets/ldtk/test.ldtk"}
+            entityRendererMap={{
+                "PlayerStart": ({entity, layer, layerPxOffsets, layerPxDimensions}) => {
+                    const worldPx = centerTilePivot(layerPxToWorldPx(entity.px as [number, number], layerPxOffsets, layerPxDimensions), layer.__gridSize);
+                    const worldPos = pxToGridPosition(worldPx, layer.__gridSize);
+
+                    return <RigidBody colliders={"hull"} position={[worldPos[0], worldPos[1], 0]}>
+                        <mesh>
+                            <boxGeometry args={[1, 1, 1]}/>
+                            <meshStandardMaterial color={"#00ff00"}/>
+                        </mesh>
+                    </RigidBody>
+                }
+            }}
+            tileRendererMap={{
+                "RECT": (props) => {
+                    return <>
+                        <TileSprite {...props}/>
+                        <TileRectCollider {...props}/>
+                    </>
+                }
+            }}
+        />
     </>
 }
