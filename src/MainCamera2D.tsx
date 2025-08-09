48ee75b6557a02a3b7300abd11f4e4a8b0261c0e
chamchi0809 <seoshi999@gmail.com>
2025-08-09 18:07:26 +0900

    fix: update camera frustum in MainCamera2D component

--- src/MainCamera2D.tsx
+++ src/MainCamera2D.tsx
@@ -1,6 +1,6 @@
-import {CameraControls, OrthographicCamera} from "@react-three/drei";
+import {CameraControls, OrthographicCamera, type OrthographicCameraProps} from "@react-three/drei";
 import {useThree} from "@react-three/fiber";
-import {useMemo} from "react";
+import {useEffect, useLayoutEffect, useMemo, useRef} from "react";
 
 export default function MainCamera2D(
     {
@@ -11,6 +11,7 @@ export default function MainCamera2D(
 ) {
 
     const {aspect} = useThree(state => state.viewport);
+    const camera = useThree(state => state.camera);
     const frustum = useMemo(() => {
         const height = size * 2;
         const width = height * aspect;
@@ -22,6 +23,17 @@ export default function MainCamera2D(
             right: width / 2,
         }
     }, [aspect, size]);
+
+    useLayoutEffect(() => {
+        if (camera) {
+            const orthographicCamera = camera as OrthographicCameraProps;
+            orthographicCamera.left = frustum.left;
+            orthographicCamera.right = frustum.right;
+            orthographicCamera.top = frustum.top;
+            orthographicCamera.bottom = frustum.bottom;
+            camera.updateProjectionMatrix();
+        }
+    }, [frustum, camera]);
 
     return <OrthographicCamera makeDefault {...frustum} position={[0, 0, 10]}>
         <CameraControls/>
