60e44b3ae1bc3606482a19ea9be4d36fe221dbc6
최지원 <66864266+chamchi0809@users.noreply.github.com>
2025-10-27 14:16:56 +0900

    add tileset id selector

--- src/views/tileset-editor/TilesetEditor.tsx
+++ src/views/tileset-editor/TilesetEditor.tsx
@@ -1,12 +1,29 @@
 import {useControls} from "leva";
 import {Html} from "@react-three/drei";
 import pathSelectPlugin from "@/common/components/leva-plugins/PathSelectPlugin.tsx";
+import {useAsync} from "react-use";
+import type {Ldtk} from "@/common/ldtk/models/LdtkTypes.ts";
 
 export default function TilesetEditor() {
 
     const {ldtkPath} = useControls({
         ldtkPath: pathSelectPlugin({path: "", disabled: false})
     })
+
+    const {value: ldtk} = useAsync(async () => {
+        if (window.api && ldtkPath.path && ldtkPath.path.endsWith(".ldtk")) {
+            const {data} = await window.api.readAbs(ldtkPath.path);
+            return JSON.parse(data) as Ldtk;
+        }
+    }, [ldtkPath.path]);
+
+    const {tilesetId} = useControls({
+        tilesetId: {
+            options: ldtk ? ldtk.defs.tilesets.map(t => t.identifier) : [],
+            disabled: !ldtk,
+            value: ldtk ? ldtk.defs.tilesets[0].identifier : "",
+        }
+    }, [ldtk]);
 
     return <>
         <mesh>
