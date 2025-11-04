eb69d4bad2a4d2b3005f3b2030dce581ff34cbdf
chamchi0809 <seoshi999@gmail.com>
2025-11-05 02:21:12 +0900

    fix eslint type error

--- eslint.config.ts
+++ eslint.config.ts
@@ -2,10 +2,10 @@ import eslint from "@eslint/js";
 import stylistic from "@stylistic/eslint-plugin";
 import reactPlugin from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
-import fs from "fs";
+import { readFileSync } from "fs";
 import tseslint from "typescript-eslint";
 
-const gitignorePatterns = fs.readFileSync(".gitignore", "utf-8")
+const gitignorePatterns = readFileSync(".gitignore", "utf-8")
   .split("\n")
   .filter(line => line && !line.startsWith("#"));
 
