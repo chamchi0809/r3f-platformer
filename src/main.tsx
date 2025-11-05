489e24fc0d574b5e764d190221bb4699184139ba
최지원 <66864266+chamchi0809@users.noreply.github.com>
2025-11-05 15:54:54 +0900

    import absolute path

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.tsx";
+import "@/index.css";
+import App from "@/App.tsx";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { chamQueryClient } from "@/common/queries/ChamQuery.ts";
