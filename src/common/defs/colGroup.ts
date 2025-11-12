c00b353fcbf377c53a2c27e478a03fc8b239c341
chamchi0809 <seoshi999@gmail.com>
2025-11-13 01:20:29 +0900

    SENSOR 콜리젼그룹 추가

--- src/common/defs/colGroup.ts
+++ src/common/defs/colGroup.ts
@@ -4,9 +4,11 @@ export type ColGroup = keyof typeof COL_GROUP;
 export const COL_GROUP = {
   DEFAULT: 0,
   CHARACTER: 1,
+  SENSOR: 2,
 } as const;
 
 export const INTERACTION_GROUPS: Record<ColGroup, number> = {
   DEFAULT: interactionGroups(COL_GROUP.DEFAULT),
   CHARACTER: interactionGroups(COL_GROUP.CHARACTER, COL_GROUP.DEFAULT),
+  SENSOR: interactionGroups(COL_GROUP.SENSOR),
 };
