84058a2db717be996409941397fdd8ec85848efa
chamchi0809 <seoshi999@gmail.com>
2025-11-13 02:35:36 +0900

    add getuvs function

--- src/common/traits/SpriteAnim.ts
+++ src/common/traits/SpriteAnim.ts
@@ -1,11 +1,18 @@
 import { trait, type World } from "koota";
 import { Elapsed } from "@/common/traits/Elapsed.ts";
 import { getTexture } from "@/common/utils/textureUtils.ts";
+import type { Texture } from "three";
 
 export interface SpriteAnimDef {
+  // start and end frame index (in ascending order)
   range: [number, number]
+  // function to get the texture path for a given frame index
   getPath: (index: number) => string
+  // function to get the UV mapping for a given frame index and total length
+  getUVs?: (index: number, length: number) => [uvw: number, uvx: number]
+  // duration of each frame in seconds
   frameDuration?: number
+  // whether the animation should loop
   loop?: boolean
 }
 
@@ -14,18 +21,21 @@ export const SpriteAnim = trait(() => null! as SpriteAnimImpl);
 export class SpriteAnimImpl implements SpriteAnimDef {
   range: [number, number];
   getPath: (index: number) => string;
+  getUVs: (index: number, length: number) => [uvw: number, uvx: number];
   current: number;
   timestamp: number;
   frameDuration = 0.1;
   loop = true;
+  private textureCache: Map<`${string}-${number}-${number}`, Texture> = new Map();
 
   constructor(
     world: World,
     def: SpriteAnimDef,
   ) {
-    const { range, getPath, frameDuration = 0.1, loop = true } = def;
+    const { range, getPath, getUVs = () => [1, 0], frameDuration = 0.1, loop = true } = def;
     this.range = range;
     this.getPath = getPath;
+    this.getUVs = getUVs;
     this.current = range[0];
     this.timestamp = world.get(Elapsed)!.value;
     this.frameDuration = frameDuration;
@@ -50,7 +60,20 @@ export class SpriteAnimImpl implements SpriteAnimDef {
   }
 
   getCurrentFrameTexture() {
-    return getTexture(this.getPath(Math.floor(this.current)));
+    const path = this.getPath(Math.floor(this.current));
+    const uvs = this.getUVs(Math.floor(this.current), this.range[1] - this.range[0] + 1);
+    const cacheKey = `${path}-${uvs[0]}-${uvs[1]}` as const;
+
+    if (this.textureCache.has(cacheKey)) {
+      return this.textureCache.get(cacheKey)!;
+    }
+    const cutTexture = getTexture(path).clone();
+    const [uvw, uvx] = uvs;
+    cutTexture.repeat.set(uvw, 1);
+    cutTexture.offset.set(uvx, 0);
+    cutTexture.needsUpdate = true;
+    this.textureCache.set(cacheKey, cutTexture);
+    return cutTexture;
   }
 
   restart() {
