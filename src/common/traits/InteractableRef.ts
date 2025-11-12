04c793c33851c751429d6c807238fcfac6073c14
chamchi0809 <seoshi999@gmail.com>
2025-11-13 01:01:11 +0900

    add InteractableRef trait

---
+++ src/common/traits/InteractableRef.ts
@@ -0,0 +1,23 @@
+import { trait } from "koota";
+import type { Collider, World } from "@dimforge/rapier2d";
+
+export const InteractableRef = trait(() => null! as Interactable);
+
+export class Interactable {
+  world: World;
+  sensor: Collider;
+
+  constructor(collider: Collider, world: World) {
+    this.world = world;
+    this.sensor = collider;
+  }
+
+  get col() {
+    return this.world.getCollider(this.sensor.handle);
+  }
+
+  isIntersecting(otherCollider: Collider) {
+    const intersection = this.world.intersectionPair(this.col, otherCollider);
+    return intersection !== null;
+  }
+}
