5aa76defe4aefc905f3af3abfb79725b48d08e37
chamchi0809 <seoshi999@gmail.com>
2025-11-19 03:06:44 +0900

    fix error

--- src/App.tsx
+++ src/App.tsx
@@ -99,8 +99,8 @@ function App() {
                   </KeyboardControls>
                 </Physics>
               </Suspense>
+              <InteractionHint />
             </Canvas>
-            <InteractionHint />
             <PauseModal />
           </GameContainer>
         )}
