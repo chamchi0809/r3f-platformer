218654c2a09461dafc9308970c3067d15ead34de
FLY123 <50164284+akfakf0509@users.noreply.github.com>
2025-11-20 00:09:02 +0900

    feat: 상호작용 힌트

--- src/common/components/InteractionHint.tsx
+++ src/common/components/InteractionHint.tsx
@@ -3,14 +3,42 @@ import { InteractableRef } from "../traits/InteractableRef";
 import { IsInteractionFocused } from "../traits/IsInteractionFocused";
 import type { Entity } from "koota";
 import { Html } from "@react-three/drei";
+import { useRef } from "react";
+import { Group } from "three";
+import styled from "styled-components";
+import { useApp } from "@/store/useAppStore";
+import { useFrame } from "@react-three/fiber";
+
+const HintContainer = styled.div`
+  padding: 8px 12px;
+  background: rgba(0, 0, 0, 0.7);
+  color: white;
+  white-space: nowrap;
+`;
 
 function HintLabel({ entity }: { entity: Entity }) {
-  // TODO: 객체 위치로 이동
-  // TODO: 상호 작용 키 가져와서 표시
+  const groupRef = useRef<Group>(null);
+  const interactable = entity.get(InteractableRef);
+
+  useFrame(() => {
+    if (!groupRef.current) return;
+    if (!interactable) return;
+    const pos = groupRef.current.position;
+    const targetPos = interactable.collider.translation();
+    groupRef.current.position.set(targetPos.x, targetPos.y, pos.z);
+  });
+
+  const { activeKeymap } = useApp();
   return (
-    <group>
+    <group ref={groupRef}>
       <Html>
-        <div>Press Interaction Key</div>
+        <HintContainer>
+          Press
+          {/* TODO: 키를 정규화할 방법 찾기 */}
+          {/* TODO: kdb 컴포넌트 구현 */}
+          {activeKeymap.find(v => v.name === "interact")?.keys}
+          Key
+        </HintContainer>
       </Html>
     </group>
   );
