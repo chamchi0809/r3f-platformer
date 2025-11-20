138658026a47099db0de73f2f36bcd30c3892442
FLY123 <50164284+akfakf0509@users.noreply.github.com>
2025-11-20 23:03:21 +0900

    feat: 상호작용 힌트

--- src/common/components/InteractionHint.tsx
+++ src/common/components/InteractionHint.tsx
@@ -1,36 +1,46 @@
-import { useApp } from "@/store/useAppStore";
 import { Html } from "@react-three/drei";
 import type { Entity } from "koota";
 import styled from "styled-components";
 import { useRefTrait } from "../hooks/ecs/useRefTrait";
 import { InteractionRef } from "../traits/InteractionRef";
+import { useApp } from "@/store/useAppStore";
 
 const HintContainer = styled.div`
   padding: 8px 12px;
+
   background: rgba(0, 0, 0, 0.7);
+
   color: white;
   white-space: nowrap;
 `;
+
 const Kbd = styled.kbd`
-  background: #222;
+  display: inline-block;
+
+  margin: 0 4px;
+  padding: 2px 6px;
   border: 1px solid #555;
   border-radius: 3px;
-  padding: 2px 6px;
+
+  background: #222;
 `;
 
 export function InteractionHintView({ entity }: { entity: Entity }) {
-  const interactionRef = useRefTrait(entity, InteractionRef);
-
-  console.log("Rendering interaction hint");
-
   const { activeKeymap } = useApp();
+  const interactKeys = activeKeymap.find(v => v.name === "interact")?.keys ?? [];
+
+  const interactionRef = useRefTrait(entity, InteractionRef);
   return (
     <group ref={interactionRef}>
       <Html center>
         <HintContainer>
           Press
-          {activeKeymap.find(v => v.name === "interact")?.keys.map(k => (<Kbd key={k}>{k}</Kbd>))}
-          Key
+          {
+            interactKeys
+              .map(k => k.replace(/^Key/, ""))
+              .map(k => (<Kbd key={k}>{k}</Kbd>))
+          }
+          to interact
         </HintContainer>
       </Html>
     </group>
