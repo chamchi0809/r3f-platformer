2620efc43fc543efccedd0e0e4a5e4c16ca8a9df
chamchi0809 <seoshi999@gmail.com>
2025-11-23 01:15:50 +0900

    prevent player movement and jumping when interacting

--- src/common/systems/pollPlayerInput.ts
+++ src/common/systems/pollPlayerInput.ts
@@ -3,10 +3,16 @@ import { IsPlayer } from "@/common/traits/IsPlayer.ts";
 import { MoveInput } from "@/common/traits/MoveInput.ts";
 import type { KeyboardControlType } from "@/common/defs/keyboardControlMap.ts";
 import { JumpInput } from "@/common/traits/JumpInput.ts";
+import { IsInteracting } from "@/common/traits/IsInteracting.ts";
 
 export default function pollPlayerInput(world: World, keys: Record<KeyboardControlType, boolean>) {
   world.query(IsPlayer, MoveInput, JumpInput)
     .updateEach(([, moveInput, jumpInput]) => {
+      if (world.queryFirst(IsInteracting)) {
+        moveInput.set(0, 0);
+        jumpInput.on = false;
+        return;
+      }
       const horizontal = (keys.right ? 1 : 0) - (keys.left ? 1 : 0);
       const vertical = (keys.up ? 1 : 0) - (keys.down ? 1 : 0);
 
