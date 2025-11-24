05765b25c5b1f06b85a6bf5cb5bb0a0a4737ffdf
owl1753 <bighammer1753@gmail.com>
2025-11-25 01:42:08 +0900

    feat: battleView ui 세팅팅

--- src/common/components/entity-views/BattleView.tsx
+++ src/common/components/entity-views/BattleView.tsx
@@ -1,3 +1,22 @@
 export default function BattleView() {
   return <></>;
+import type { Entity } from "koota";
+import { useTrait } from "koota/react";
+import { IsInteracting } from "@/common/traits/IsInteracting.ts";
+import { Html } from "@react-three/drei";
+import { useRefTrait } from "@/common/hooks/ecs/useRefTrait";
+import { BattleViewRef } from "@/common/traits/BattleViewRef";
+export default function BattleView({ entity }: { entity: Entity }) {
+  const type = useTrait(entity, IsInteracting)?.type;
+  const battleViewRef = useRefTrait(entity, BattleViewRef);
+
+  if (type !== "battle") return <></>;
+
+  return (
+    <group ref={battleViewRef}>
+      <Html fullscreen>
+        <h1>hello</h1>
+      </Html>
+    </group>
+  );
 }
