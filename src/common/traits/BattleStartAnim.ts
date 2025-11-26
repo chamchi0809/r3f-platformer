81c8d98009bbe75e54305a5a8f1a265baba70bc8
chamchi0809 <seoshi999@gmail.com>
2025-11-27 00:22:22 +0900

    change BattleStartAnim trait schema

--- src/common/traits/BattleStartAnim.ts
+++ src/common/traits/BattleStartAnim.ts
@@ -1,4 +1,4 @@
 import { trait } from "koota";
 import { createSpriteAnimDef } from "@/common/traits/SpriteAnim.ts";
 
-export const BattleStartAnim = trait({ ...createSpriteAnimDef() });
+export const BattleStartAnim = trait({ ...createSpriteAnimDef(), playedOnce: false, playedReverse: false });
